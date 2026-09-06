0b57061efe99e80e28182fff521615ceb2a59e2e
allen <allenwang513@gmail.com>
2026-09-06 15:58:50 +0800

    refractor main.rs for future maintainability/upgradeability/scaleability or whatever

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,29 @@ use std::io::{BufWriter, Write};
 use crate::vec3::{Vec3, Point3, Color};
 use crate::ray::Ray;
 
-fn main() {
-    let width: i32 = 256;
-    let height: i32 = 256;
-    let file = File::create("image.ppm").unwrap();
-    let mut writer = BufWriter::new(file);
+const width: i32 = 512;
+const height: i32 = 512;
+
+fn ray_color(ray: &Ray) -> Color {
+    let rd_normalised = ray.direction.normalised();
+    let t = 0.5 * (rd_normalised.y + 1.0);
+    (Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t)
+}
+
+fn write_header(writer: &mut BufWriter<File>){
     writeln!(writer, "P3").unwrap();
     writeln!(writer, "{width} {height}").unwrap();
     writeln!(writer, "255").unwrap();
-    // let b = (0.25 * 255.0 as f64).round() as i32;
-    // for i in 0..height{
-    //     for j in 0..width{
-    //         let r = (i as f64 / (height - 1) as f64 * 255.0).round() as i32;
-    //         let g = (j as f64 / (width - 1) as f64 * 255.0).round() as i32;
+}
 
-    //         writeln!(writer, "{r} {g} {b}").unwrap();
-    //     }
-    // }
+fn write_color(writer: &mut BufWriter<File>, color:Color){
+    writeln!(writer, "{} {} {}", ((color.x*255.0) as i32), ((color.y*255.0) as i32), ((color.z*255.0) as i32)).unwrap();
+}
+
+fn main() {
+    let file = File::create("image.ppm").unwrap();
+    let mut writer = BufWriter::new(file);
+    write_header(&mut writer);
 
     let cam_center = Point3::new(0.0, 0.0, 0.0);
     let focal_length = 2.0;
@@ -43,13 +49,10 @@ fn main() {
             let pixel_center = upper_left_pixel + pixel_delta_u * (j as f64) + pixel_delta_v * (i as f64);
 
             let ray_direction = pixel_center - cam_center;
-            let rd_normalised = ray_direction.normalised();
+            let ray = Ray::new(cam_center, ray_direction);
 
-            let t = 0.5 * (rd_normalised.y + 1.0);
-            let color = Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t;
-
-            let ray = Ray::new(cam_center, pixel_center-cam_center);
-            writeln!(writer, "{} {} {}", ((color.x*255.0) as i32), ((color.y*255.0) as i32), ((color.z*255.0) as i32)).unwrap();
+            let color = ray_color(&ray);
+            write_color(&mut writer, color);
         }
     }
 }
